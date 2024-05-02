925fee50540313e735451cd43bc43d20c40ecf0b
Frank Vanbever <frank.vanbever@mind.be>
2024-05-02 08:42:38 +0200

    Re-organize repository
    I decided to re-use this repo for all my DSP related notebooks, so not
    only for the Mathematics of the DFT. I made the following changes:
    
     - Put the notebooks in per-book folders
     - move from a requirements.txt based venv to Poetry
     - Push changes for the simulated sine wave analysis notebook

--- setup-venv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-python3 -m venv dft-mathematics-venv
-source dft-mathematics-venv/bin/activate
-pip3 install -r requirements.txt
-
--- start-lab.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-source dft-mathematics-venv/bin/activate
-jupyter lab
